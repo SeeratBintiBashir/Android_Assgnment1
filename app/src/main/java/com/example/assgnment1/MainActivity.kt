c1554401b9918388cd26420be74e7631c5eec769
mehraj <mehraj@cdse-it.com>
2023-09-10 14:59:58 +0300

    Fixed some ui and flow

--- app/src/main/java/com/example/assgnment1/MainActivity.kt
+++ app/src/main/java/com/example/assgnment1/MainActivity.kt
@@ -10,7 +10,9 @@ import android.view.View.OnClickListener
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -21,14 +23,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editAddress: EditText
     private lateinit var submitButton: Button
     private lateinit var closeButton: Button
+    private  lateinit var formLayout:LinearLayout
+    private  lateinit var viewLayout:LinearLayout
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        editName = findViewById(R.id.name)
+        formLayout= findViewById(R.id.l1)
+        viewLayout= findViewById(R.id.l2)
+        editName = findViewById(R.id.fullName)
         editPhoneNumber = findViewById(R.id.phoneNumber)
         editAddress = findViewById(R.id.address)
         submitButton = findViewById(R.id.submitButton)
@@ -38,34 +43,70 @@ class MainActivity : AppCompatActivity() {
         submitButton.text = getString(R.string.my_button_text)
 
         submitButton.setOnClickListener {
-            val name = editName.text.toString()
-            val phoneNumber = editPhoneNumber.text.toString()
-            val address = editAddress.text.toString()
-
-
-            val userDetails = "Name: $name\n Phone Number: $phoneNumber\n Address: $address"
-            myTextView.text = userDetails
-
-            editName.visibility = INVISIBLE
-            editPhoneNumber.visibility = INVISIBLE
-            editAddress.visibility = INVISIBLE
-            submitButton.visibility = INVISIBLE
-            closeButton.visibility = VISIBLE
+            val formStatus=isFormValid();
+            if(formStatus==true) {
+                showDetail()
+            }
         }
 
         closeButton.setOnClickListener {
-            myTextView.text = ""
+            showForm()
 
-            editName.text.clear()
-            editPhoneNumber.text.clear()
-            editAddress.text.clear()
-            editName.visibility = VISIBLE
-            editPhoneNumber.visibility = VISIBLE
-            editAddress.visibility = VISIBLE
-            submitButton.visibility = VISIBLE
-            closeButton.visibility = INVISIBLE
 
         }
     }
+
+    fun showForm()
+    {
+        formLayout.visibility=View.VISIBLE
+        viewLayout.visibility=View.GONE
+        myTextView.text = ""
+
+        editName.text.clear()
+        editPhoneNumber.text.clear()
+        editAddress.text.clear()
+    }
+
+    fun showDetail()
+    {
+        formLayout.visibility=View.GONE
+        viewLayout.visibility=View.VISIBLE
+
+        val name = editName.text.toString()
+        val phoneNumber = editPhoneNumber.text.toString()
+        val address = editAddress.text.toString()
+        val userDetails = "Name: $name\n Phone Number: $phoneNumber\n Address: $address"
+        myTextView.text = userDetails
+
+    }
+
+    fun isFormValid():Boolean{
+        if(editName.text.toString().isEmpty())
+        {
+            showMessage("Please Enter Your Full Name")
+            return  false;
+        }
+
+        if(editPhoneNumber.text.toString().isEmpty())
+        {
+            showMessage("Please Enter Your Phone Number")
+            return  false;
+        }
+        if(editAddress.text.toString().isEmpty())
+        {
+            showMessage("Please Enter Your Address")
+            return  false;
+        }
+
+
+
+
+        return  true;
+    }
+
+    fun showMessage(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
 }
 
